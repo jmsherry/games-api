87845e66a3c74f567ab9bdf60f69921940816d8a
James Sherry <james.m.sherry@googlemail.com>
2023-03-22 01:09:57 +0000

    added cors

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 module.exports = (app) => {
   // view engine setup
@@ -13,8 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-
-
-  
 }
