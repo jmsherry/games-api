44eca28a632e8c6c31ef9f866fffe62c353ce251
James Sherry <james.m.sherry@googlemail.com>
2023-04-05 13:57:57 +0100

    formatting

--- middleware.js
+++ middleware.js
@@ -6,14 +6,13 @@ const cors = require('cors');
 
 module.exports = (app) => {
   // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(cors());
 }
