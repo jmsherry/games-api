30ac9f0117d38628b7324fcd58b57d4721199443
James Sherry <james.m.sherry@googlemail.com>
2023-04-05 14:05:13 +0100

    more formatting

--- routes/games.js
+++ routes/games.js
@@ -1,27 +1,32 @@
-const express = require('express');
-const { body } = require('express-validator')
+const express = require("express");
+const { body } = require("express-validator");
 
-const {getGames, addGame, updateGame, deleteGame} = require('./../controllers/game.controller.js')
+const {
+  getGames,
+  addGame,
+  updateGame,
+  deleteGame,
+} = require("./../controllers/game.controller.js");
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get(`/:id?`, getGames);
-
-router.post(`/`,
-body('title').not().isEmpty().trim().escape(),
-body('games_console').not().isEmpty().trim().escape(),
-body('cover_url').trim().isURL(),
-addGame
-);
-
-router.put(`/:id`, 
-body('title').optional().not().isEmpty().trim().escape(),
-body('games_console').optional().not().isEmpty().trim().escape(),
-body('cover_url').optional().trim().isURL(),
-updateGame
-);
-
-router.delete(`/:id`, deleteGame);
+router
+  .get(`/:id?`, getGames)
+  .post(
+    `/`,
+    body("title").not().isEmpty().trim().escape(),
+    body("games_console").not().isEmpty().trim().escape(),
+    body("cover_url").trim().isURL(),
+    addGame
+  )
+  .put(
+    `/:id`,
+    body("title").optional().not().isEmpty().trim().escape(),
+    body("games_console").optional().not().isEmpty().trim().escape(),
+    body("cover_url").optional().trim().isURL(),
+    updateGame
+  )
+  .delete(`/:id`, deleteGame);
 
 module.exports = router;
